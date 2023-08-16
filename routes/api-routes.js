6901e4fc357e430374f3b6c6149640ece87210b7
Denisse Holgersen arredondo <132967474+Holgersend@users.noreply.github.com>
2023-08-15 21:36:19 -0500

    updated-api-routes.js
    added the GET and POST routes for retrieving and adding a net note

---
+++ routes/api-routes.js
@@ -0,0 +1,18 @@
+//importing dependencies
+const fs = require('fs');
+
+app.get('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
+    res.json(notes);
+});
+
+//POST route for adding new note
+app.post('/api/notes', (req, res) => {
+    const newNote = req.body;
+    const notes = JSON.parse(fs.readFileSync('db.json', 'utf-8'));
+    newNote.id = notes.uuid();
+    notes.push(newNote);
+
+    fs.writeFileSync('db.json', JSON.stringify(notes));
+}); 
+
